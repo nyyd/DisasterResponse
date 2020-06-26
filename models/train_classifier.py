e6c3b300004bd099beac1f7f491f5b583a1ad344
nyyd <34407299+nyyd@users.noreply.github.com>
2020-06-25 23:13:14 -0400

    Add files via upload

---
+++ models/train_classifier.py
@@ -0,0 +1,107 @@
+import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn import multioutput
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.grid_search import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import precision_recall_fscore_support
+import pickle
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+def load_data(database_filepath):
+    
+    """Load the filepath and return the data"""
+    name = 'sqlite:///' + database_filepath
+    engine = create_engine(name)
+    df = pd.read_sql_table('df', con=engine) # is table always called this? 
+    print(df.head())
+    X = df['message']
+    y = df[df.columns[4:]]
+    category_names = y.columns
+    return X, y, category_names
+
+#word processing
+def tokenize(text):
+    tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    processed_txt = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        processed_txt.append(clean_tok)
+    return processed_txt
+
+#model instantiation and training
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier (RandomForestClassifier()))
+        ])
+    model=pipeline.fit(X_train, y_train)
+    return model
+
+#model evaluation
+def evaluate_model(model, X_test, Y_test, category_names):
+    test_pred = model.predict (X_test)
+    avrg_results(true, pred):
+    results = pd.DataFrame(columns=['Category', 'f_score', 'precision', 'recall'])
+    num = 0
+    #model scoring
+    for cat in y_test.columns:
+        precision, recall, f_score, support = precision_recall_fscore_support(true[cat], pred[:,num], average='weighted')
+        results.set_value(num+1, 'Category', cat)
+        results.set_value(num+1, 'f_score', f_score)
+        results.set_value(num+1, 'precision', precision)
+        results.set_value(num+1, 'recall', recall)
+        num += 1
+    print('Average f_score:', results['f_score'].mean())
+    print('AVerage precision:', results['precision'].mean())
+    print('Average recall:', results['recall'].mean())
+
+    #save tarined model
+def save_model(model, model_filepath):
+        pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
